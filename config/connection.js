6cf4ca59b11ae5ddc79c8ec47b105b7ba6cb9659
gstroup11 <gstroup11@gmail.com>
2023-10-15 19:08:52 -0400

    This push pushes file structure, env, connection, and schema files.  This enables the connection to the database and creates the employees database.

---
+++ config/connection.js
@@ -0,0 +1,18 @@
+const mysql = require("mysql2");
+
+require('dotenv').config();
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+
+connection.connect(function (err) {
+  if (err) throw err;
+});
+
+module.exports = connection;
